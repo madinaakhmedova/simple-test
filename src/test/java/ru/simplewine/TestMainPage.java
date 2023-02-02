f63164ef26ba8b23745f4b0d88a6b1a262c5cff4
Madina Akhmedova <akhmedovamadina11@gmail.com>
2023-02-02 23:24:17 +0300

    click first banner on main page - fix

--- src/test/java/ru/simplewine/TestMainPage.java
+++ src/test/java/ru/simplewine/TestMainPage.java
@@ -2,23 +2,29 @@ package ru.simplewine;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.webdriver;
-import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class TestMainPage extends TestBase {
 
     @Test
     void clickFirstBanner() {
         mainPage.openPage();
+
         $(".age-confirm__button.js-age-confirm").click();
+//        $("[data-autotest-target-id=city-popup-city-agree-btn").click();
 
         mainPage.banners.get(0).shouldNotBe(Condition.visible);
         mainPage.banners.get(1).shouldBe(Condition.visible).click();
 
-        webdriver().shouldNotHave(url(baseUrl));
+        String url = webdriver().driver().getCurrentFrameUrl();
+        String secondTabUrl = webdriver().driver().switchTo().window(1).getCurrentUrl();
+
+        Assertions.assertNotEquals(secondTabUrl, "https://simplewine.ru");
+        Assertions.assertNotEquals(secondTabUrl, "https://simplewine.ru/");
 
         Selenide.sleep(5000);
     }
